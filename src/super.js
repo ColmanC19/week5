c30029332821ff500a6b6e055ea7796ea45d5f64
Colman Currie <curriecolman@gmail.com>
2019-11-15 12:35:15 -0800

    update js

--- src/super.js
+++ src/super.js
@@ -1,8 +1,24 @@
-export function Age(years) {
+export function Age(mercury, venus, mars, jupiter) {
   this.years= years;
 }
 
-Age.prototype.change = function() {
+Age.prototype.mercury = function() {
+  let (this.years*.24);
+  return this.years;
+}
+Age.prototype.venus = function() {
+  let (this.years*.62);
+  return this.years;
+}
+Age.prototype.mars = function() {
+  let (this.years*1.88);
+  return this.years;
+}
+Age.prototype.jupiter = function() {
+  let (this.years*11.86);
+  return this.years;
+}
+Age.prototype.expectancy = function() {
   let (this.years*.24);
   return this.years;
 }

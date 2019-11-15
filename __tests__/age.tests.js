85124561c83efa1f4a97c1fae2e9848ce71a435b
Colman Currie <curriecolman@gmail.com>
2019-11-15 11:58:55 -0800

    add js

---
+++ __tests__/age.tests.js
@@ -0,0 +1,28 @@
+import { Age } from './../src/super.js';
+describe("Age", () => {
+test("should correctly multiply the user age by 0.24, () => "{
+  var age = new age(10);
+  expect(this.years).toEqual(2.4);
+})
+test("should correctly multiply the user age by .62, () => "{
+  var age = new age(10);
+  expect(this.years).toEqual(6.2);
+})
+test("should correctly multiply the user age by 1.88, () => "{
+  var age = new age(10);
+  expect(this.years).toEqual(18.8);
+})
+test("should correctly multiply the user age by 11.86, () => "{
+  var age = new age(10);
+  expect(this.years).toEqual(118.6);
+})
+
+
+
+
+
+
+
+
+
+};
--- src/super.js
+++ src/super.js
@@ -1,3 +1,8 @@
 export function Age(years) {
   this.years= years;
 }
+
+Age.prototype.change = function() {
+  let (this.years*.24);
+  return this.years;
+}

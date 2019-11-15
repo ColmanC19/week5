63a7140e034d5980be34cb5235c74e84e04c9115
Colman Currie <curriecolman@gmail.com>
2019-11-15 14:42:12 -0800

    update testing

--- __tests__/age.tests.js
+++ __tests__/age.tests.js
@@ -1,29 +1,20 @@
 import { Age } from './../src/super.js';
 
 describe("Age", () => {
-test("should correctly multiply the user age by 0.24, () => "{
+test("should correctly multiply the user age by 0.24", () => {
   var age = new age(10);
   expect(this.years).toEqual(2.4);
-})
-test("should correctly multiply the user age by .62, () => "{
+});
+test("should correctly multiply the user age by .62", () => {
   var age = new age(10);
   expect(this.years).toEqual(6.2);
-})
-test("should correctly multiply the user age by 1.88, () => "{
+});
+test("should correctly multiply the user age by 1.88", () => {
   var age = new age(10);
   expect(this.years).toEqual(18.8);
-})
-test("should correctly multiply the user age by 11.86, () => "{
+});
+test("should correctly multiply the user age by 11.86", () => {
   var age = new age(10);
   expect(this.years).toEqual(118.6);
-})
-
-
-
-
-
-
-
-
-
-};
+});
+});
--- src/super.js
+++ src/super.js
@@ -3,22 +3,30 @@ export function Age(mercury, venus, mars, jupiter) {
 }
 
 Age.prototype.mercury = function() {
-  let (this.years*.24);
-  return this.years;
+  if (this.years*.24){
+    let this.years;
+  }
+
 }
 Age.prototype.venus = function() {
-  let (this.years*.62);
-  return this.years;
+  if (this.years*.62){
+    let this.years;
+  }
 }
 Age.prototype.mars = function() {
-  let (this.years*1.88);
-  return this.years;
+  if (this.years*1.88){
+    let this.years;
+  }
+
 }
 Age.prototype.jupiter = function() {
-  let (this.years*11.86);
-  return this.years;
+  if (this.years*11.86){
+    let this.years;
+  }
+
 }
 Age.prototype.expectancy = function() {
-  let (this.years*.24);
-  return this.years;
-}
+  if (this.years*.24){
+    let this.years;
+  }
+};

3a553247b9da928e86ac6aef13d05c65215e3aad
Colman Currie <curriecolman@gmail.com>
2019-11-17 15:13:57 -0800

    get me on the right track with constructors

--- __tests__/age.tests.js
+++ __tests__/age.tests.js
@@ -1,20 +1,23 @@
 import { Age } from './../src/super.js';
 
 describe("Age", () => {
-test("should correctly multiply the user age by 0.24", () => {
-  var age = new age(10);
-  expect(this.years).toEqual(2.4);
-});
-test("should correctly multiply the user age by .62", () => {
-  var age = new age(10);
-  expect(this.years).toEqual(6.2);
-});
-test("should correctly multiply the user age by 1.88", () => {
-  var age = new age(10);
-  expect(this.years).toEqual(18.8);
-});
-test("should correctly multiply the user age by 11.86", () => {
-  var age = new age(10);
-  expect(this.years).toEqual(118.6);
-});
+  test("should correctly multiply the user age by 0.24", () => {
+    let age = new Age(10);
+    expect(age.years).toEqual(10);
+    let mercuryAge = age.mercury();
+    console.log(mercuryAge);
+    expect(mercuryAge).toEqual(2.4);
+  });
+  // test("should correctly multiply the user age by .62", () => {
+  //   var age = new Age(10);
+  //   expect(this.years).toEqual(6.2);
+  // });
+  // test("should correctly multiply the user age by 1.88", () => {
+  //   var age = new Age(10);
+  //   expect(this.years).toEqual(18.8);
+  // });
+  // test("should correctly multiply the user age by 11.86", () => {
+  //   var age = new Age(10);
+  //   expect(this.years).toEqual(118.6);
+  // });
 });
--- src/super.js
+++ src/super.js
@@ -1,43 +1,41 @@
-export function Age(years, mercury, venus, mars, jupiter) {
-  this.years= years;
-  this.mercury = mercury;
-  this.venus = venus;
-  this.mars = mars;
-  this.jupiter = jupiter;
+export class Age {
+  constructor(years) {
+    this.years = years;
+    // this.mercury = mercury;
+    // this.venus = venus;
+    // this.mars = mars;
+    // this.jupiter = jupiter;
+   }
+  // }
+  // const yearsOld = 0;
+  // const expectancy = 77;
 
-}
-const yearsOld = ;
-const expectancy = ;
-
-Age.prototype.mercury = function() {
-  const mercuryAge = .24;
-  let years = this.years*.24;
-  return years;
+  mercury() {
+    const mercuryAge = .24;
+    let years = (this.years*.24);
+    return years;
   }
 
-}
-Age.prototype.venus = function() {
-  const venusAge = .62;
-  let years = this.years*.62;
-  return years;
-  }
-}
-Age.prototype.mars = function() {
-  const marsAge = 1.88;
-  let years = this.years*1.88;
-  return years;
-  }
-
-}
-Age.prototype.jupiter = function() {
-  const venusAge = 11.86;
-  let years = this.years*11.86;
-  return years;
-  }
-
-}
-Age.prototype.expectancy = function() {
-  if (true){
-    (yearsOld-expectancy);
-  }
+  // Age.prototype.venus = function() {
+  //   const venusAge = .62;
+  //   let years = this.years*.62;
+  //   return years;
+  //   }
+  // Age.prototype.mars = function() {
+  //   const marsAge = 1.88;
+  //   let years = this.years*1.88;
+  //   return years;
+  //   }
+  //
+  // Age.prototype.jupiter = function() {
+  //   const venusAge = 11.86;
+  //   let years = this.years*11.86;
+  //   return years;
+  //   }
+  //
+  // Age.prototype.expectancy = function() {
+  //   if (this.years > 1){
+  //     (yearsOld-expectancy);
+  //   }
+  //  }
 };
